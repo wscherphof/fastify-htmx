f1057923cf4f15d7fe6ef20b0604e8d80362791f
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-15 20:50:52 +0100

    fix for refresh html & push w/o content

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ const fs = require('fs')
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 
-async function plugin(fastify, options = {}) {
+async function plugin (fastify, options = {}) {
   const defaults = {
     dist: path.resolve(process.cwd(), 'vite', 'dist'),
     origin: {
       protocol: 'http',
       address: 'localhost',
       port: '3001'
-    }
+    },
+    appUrl: '/app',
+    mainSelector: 'main'
   }
   options = Object.assign(defaults, options)
-  const { dist, origin } = options
+  const { dist, origin, appUrl, mainSelector } = options
 
   // allow requests from the app dev server
   fastify.register(require('fastify-cors'), {
@@ -33,8 +35,7 @@ async function plugin(fastify, options = {}) {
     root: dist
   })
 
-  // serve the full page HTML when not Ajax
-  fastify.addHook('onRequest', (request, reply, done) => {
+  fastify.addHook('onRequest', function fullPageHtml (request, reply, done) {
     const { url, headers } = request
     const isFileName = url.match(/\.\w+$/) // .js, .css, ...
     const hxRequest = headers['hx-request']
@@ -47,10 +48,35 @@ async function plugin(fastify, options = {}) {
     done()
   })
 
-  fastify.decorateReply('hxRedirect', function hxRedirect(path) {
-    this.header('HX-Redirect', path)
-    return 'redirect'
+  fastify.addHook('onRequest', function push (request, reply, done) {
+    const PUSH = '/push'
+    const { url } = request
+    if (url === PUSH || url.startsWith(`${PUSH}/`)) {
+      // push to the rest of the url
+      const destination = url.replace(new RegExp(`^\\${PUSH}`), '')
+      reply.header('HX-Push', destination)
+      reply.send()
+    }
+    done()
   })
+
+  fastify.addHook('onSend', async function ensureUrlTrigger (request, reply, payload) {
+    if (pathname(request) === appUrl) {
+      const url = decodeURIComponent(request.query.url)
+      return payload + `
+        <span hx-get="${url}" hx-target="${mainSelector}"></span>
+      `
+    }
+  })
+
+  fastify.decorateReply('hxRedirect', function hxRedirect (path) {
+    this.header('HX-Redirect', path)
+    this.send()
+  })
+
+  function pathname (request) {
+    return request.url.split('?')[0]
+  }
 }
 
 module.exports = fp(plugin, { name: 'fastify-htmx' })
