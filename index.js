259cbd12757ecff2f608ada1bf712ffa318b6230
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-16 23:19:54 +0100

    render pushed urls server side

--- index.js
+++ index.js
@@ -2,28 +2,20 @@
 
 const fp = require('fastify-plugin')
 const path = require('path')
-const fs = require('fs')
 
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 
 async function plugin (fastify, options = {}) {
   const defaults = {
-    dist: path.resolve(process.cwd(), 'vite', 'dist'),
-    origin: {
-      protocol: 'http',
-      address: 'localhost',
-      port: '3001'
-    },
-    appUrl: '/app',
-    mainSelector: 'main'
+    dist: path.resolve('vite', 'dist'),
+    origin: 'http://localhost:3001'
   }
   options = Object.assign(defaults, options)
-  const { dist, origin, appUrl, mainSelector } = options
 
   // allow requests from the app dev server
   fastify.register(require('fastify-cors'), {
-    origin: `${origin.protocol}://${origin.address}:${origin.port}`,
+    origin: options.origin,
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'HEAD'],
     allowedHeaders: ['Content-Type', 'Authorization', 'HX-Boosted', 'HX-Current-URL', 'HX-History-Restore-Request', 'HX-Prompt', 'HX-Request', 'HX-Target', 'HX-Trigger', 'HX-Trigger-Name'],
     exposedHeaders: ['HX-Push', 'HX-Redirect', 'HX-Refresh', 'HX-Retarget', 'HX-Trigger', 'HX-Trigger-After-Swap', 'HX-Trigger-After-Settle'],
@@ -32,22 +24,24 @@ async function plugin (fastify, options = {}) {
 
   // serve the dist as the root
   fastify.register(require('fastify-static'), {
-    root: dist
+    root: options.dist,
+    send: {
+      index: false
+    }
   })
 
-  fastify.addHook('onRequest', function fullPageHtml (request, reply, done) {
+  fastify.addHook('onRequest', function fullPageHook (request, reply, done) {
     const { url, headers } = request
     const isFileName = url.match(/\.\w+$/) // .js, .css, ...
     const hxRequest = headers['hx-request']
     const hxHistoryRestoreRequest = headers['hx-history-restore-request']
     if (!isFileName && (!hxRequest || hxHistoryRestoreRequest)) {
-      const indexHtml = path.join(dist, 'index.html')
-      reply.header('Content-Type', 'text/html')
-      reply.send(fs.createReadStream(indexHtml, 'utf8'))
+      reply.sendFile('index.html', options.dist)
     }
     done()
   })
 
+  // this can probably be fastify.get('/push*') or something
   fastify.addHook('onRequest', function push (request, reply, done) {
     const PUSH = '/push'
     const { url } = request
@@ -60,23 +54,10 @@ async function plugin (fastify, options = {}) {
     done()
   })
 
-  fastify.addHook('onSend', async function ensureUrlTrigger (request, reply, payload) {
-    if (pathname(request) === appUrl) {
-      const url = decodeURIComponent(request.query.url)
-      return payload + `
-        <span hx-get="${url}" hx-target="${mainSelector}"></span>
-      `
-    }
-  })
-
   fastify.decorateReply('hxRedirect', function hxRedirect (path) {
     this.header('HX-Redirect', path)
     this.send()
   })
-
-  function pathname (request) {
-    return request.url.split('?')[0]
-  }
 }
 
 module.exports = fp(plugin, { name: 'fastify-htmx' })
